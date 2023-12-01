ad8385ae5305020914d57e43d9cf82534bad99b7
wildflu <walidcode0@gmail.com>
2023-12-01 20:57:33 +0000

    use FAB for sort options

--- lib/components/user_comp.dart
+++ lib/components/user_comp.dart
@@ -7,39 +7,59 @@ import '../model/user.dart';
 class UserComp extends StatelessWidget {
   const UserComp({super.key, required this.user});
   final User user;
-  
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(10),
+      height: 20,
+      padding: const EdgeInsets.all(5),
       margin: const EdgeInsets.all(10),
       decoration: BoxDecoration(
         border: Border.all(width: 2, color: Colors.black12),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+
+      child: Stack(
         children: [
-          Row(
-            children: [
-              Text('UserName :',style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),),
-              Text(user.name),
-            ],
+          Positioned(
+            top: 10,
+            child: Text(user.name, style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, letterSpacing: 2, ),)
           ),
-          Row(
-            children: [
-              Text('friends :',style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),),
-              Text('${user.friends.join(', ')}'),
-            ],
-          ),
-          Row(
-            children: [
-              Text('interast :',style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),),
-              Text('${user.interests.join(', ')}'),
-            ],
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.greenAccent,
+                    Colors.white
+                  ]
+                )
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path_0 = Path();
+    path_0.moveTo(size.width*-0.0016500,size.height*0.4002000);
+    path_0.quadraticBezierTo(size.width*0.4974500,size.height*0.6510400,size.width*1.0016500,size.height*0.4000400);
+    path_0.quadraticBezierTo(size.width*1.0020750,size.height*0.5503700,size.width*1.0033500,size.height*1.0013600);
+    path_0.lineTo(size.width*-0.0016500,size.height*1.0013600);
+
+    return path_0;
+
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
+}
+
+
+
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:get/get.dart';
 import 'package:testn/model/user.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
@@ -31,9 +32,32 @@ class HomePage extends StatelessWidget {
         toolbarHeight: 70,
         title: const Text("Test"),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => userDB.filterByInterests(['travel']),
-        label: const Text('sorted Users')
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.ellipsis_search,
+        animationDuration: const Duration(milliseconds: 700),
+        spacing: 15,
+        spaceBetweenChildren: 8,
+        children: [
+          SpeedDialChild(
+            onTap: (){},
+            child: const Icon(Icons.nest_cam_wired_stand_outlined),
+            label: "name",
+          ),
+          SpeedDialChild(
+            onTap: (){},
+            child: const Icon(Icons.face_retouching_natural),
+            label: "freind",
+          ),
+          SpeedDialChild(
+            onTap: (){},
+            child: const Icon(Icons.interests_outlined),
+            label: "Interests",
+          )
+        ],
+        child: FloatingActionButton(
+          onPressed: () => userDB.filterByInterests(['travel']),
+          child: const Icon(Icons.sort),
+        ),
       ),
       body: GetBuilder<UserData>(
           init: UserData(),
@@ -43,8 +67,9 @@ class HomePage extends StatelessWidget {
             enablePullDown: true,
             enablePullUp: true,
             controller: _refreshController,
-            header: WaterDropHeader(),
-            child: ListView.builder(
+            header: const WaterDropHeader(),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 1, childAspectRatio: 0.8),
               itemCount: userDB.userData.length,
               itemBuilder: (context, index) {
                 return UserComp(user: userDB.userData[index]);
